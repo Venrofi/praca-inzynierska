7faaf277e007820673c022426d2393f384c39b62
Venrofi <radoslaw.terelak@gmail.com>
2023-10-11 22:36:23 +0200

    [FE] Homepage - Initial implementation

--- wortal-app/src/app/modules/homepage/components/homepage/homepage.component.ts
+++ wortal-app/src/app/modules/homepage/components/homepage/homepage.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
 })
 export class HomepageComponent {
+
+  isWideScreen: boolean = window.innerWidth > 1000;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.isWideScreen = window.innerWidth > 1000;
+  }
 
 }
--- wortal-app/src/app/modules/homepage/homepage.module.ts
+++ wortal-app/src/app/modules/homepage/homepage.module.ts
@@ -4,6 +4,18 @@ import { CommonModule } from '@angular/common';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { SharedModule } from '../../shared/shared.module';
 
+import { FormsModule } from '@angular/forms';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatMenuModule } from '@angular/material/menu';
+
 @NgModule({
   declarations: [
     HomepageComponent
@@ -11,6 +23,17 @@ import { SharedModule } from '../../shared/shared.module';
   imports: [
     CommonModule,
     SharedModule,
+    FormsModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatMenuModule,
+    MatProgressSpinnerModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
   ],
   exports: [
     HomepageComponent
