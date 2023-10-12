4d7c6d925e25ffb977a9b03c0534bd522e6493cf
Venrofi <radoslaw.terelak@gmail.com>
2023-10-12 21:49:37 +0200

    [FE] - Initial Discussion List & Side section

---
+++ wortal-app/src/app/modules/homepage/components/discussion-list/discussion-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-disscussion-list',
+  templateUrl: './discussion-list.component.html',
+})
+export class DiscussionListComponent {
+
+}
--- wortal-app/src/app/modules/homepage/homepage.module.ts
+++ wortal-app/src/app/modules/homepage/homepage.module.ts
@@ -2,23 +2,27 @@ import { NgModule } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { DiscussionListComponent } from './components/discussion-list/discussion-list.component';
 import { SharedModule } from '../../shared/shared.module';
 
 import { FormsModule } from '@angular/forms';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
-    HomepageComponent
+    HomepageComponent,
+    DiscussionListComponent
   ],
   imports: [
     CommonModule,
@@ -27,10 +31,12 @@ import { MatMenuModule } from '@angular/material/menu';
     MatButtonModule,
     MatButtonToggleModule,
     MatCardModule,
+    MatDividerModule,
     MatIconModule,
     MatInputModule,
     MatMenuModule,
     MatProgressSpinnerModule,
+    MatProgressBarModule,
     MatTabsModule,
     MatToolbarModule,
     MatTooltipModule,
