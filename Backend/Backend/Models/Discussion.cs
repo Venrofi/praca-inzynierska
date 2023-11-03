dbb550375dbac0e56204ed0860fc16e668c53293
Kamil Plewnia <plewnia.k@e80group.com>
2023-11-03 08:27:14 +0100

    [BE] Add: add discussions

---
+++ Backend/Backend/Models/Discussion.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
+{
+    public class Discussion
+    {
+        [Required]
+        public Guid DiscussionId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string InitialText { get; set; }
+
+        public User User { get; set; }
+        public Guid UserId { get; set; }
+        public List<Post> Posts { get; set; }
+    }
+}
