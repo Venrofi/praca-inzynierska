00dbd0efa456a161859683331f4a900edafe763e
Kamil Plewnia <plewnia.k@e80group.com>
2023-11-03 16:09:10 +0100

    [BE] Add: add start class for LoginRegisterController

---
+++ Backend/Backend/Controllers/LoginRegisterController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginRegisterController : ControllerBase
+    {
+
+
+
+    }
+}
