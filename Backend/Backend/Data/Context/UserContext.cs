a315735a2b78fef88674915e17d73e8fe8bf10fd
Kamil Plewnia <plewnia.k@e80group.com>
2023-11-03 16:07:07 +0100

    [BE] Change: changes in folders tree

---
+++ Backend/Backend/Data/Context/UserContext.cs
@@ -0,0 +1,31 @@
+﻿using Backend.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Data.Context
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; } //= null!;
+        public DbSet<Comment> Comments { get; set; } //= null!;
+        public DbSet<DiscussionPost> DiscossionPosts { get; set; }
+        public DbSet<DiscussionPostDetails> DiscossionPostsDetails { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<PremiereAlbum> PremiereAlbums { get; set; }
+        public DbSet<PremiereAlbumDetails> PremiereAlbumDetails { get; set; }
+        public DbSet<Track> Tracks { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
