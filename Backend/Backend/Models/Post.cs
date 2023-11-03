1893c63b78d4e1ff3b6f23f056b84a97ed847ce7
Kamil Plewnia <plewnia.k@e80group.com>
2023-11-03 08:26:51 +0100

    [BE] Add: add Post

---
+++ Backend/Backend/Models/Post.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
+{
+    public class Post
+    {
+        [Required]
+        public Guid PostId { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Text { get; set; }
+
+        [Required]
+        public int Reactions { get; set; }
+
+        [DataType(DataType.Url)]
+        public string Url { get; set; }
+
+        public User User { get; set; }
+        public Guid UserId { get; set; }
+
+        public Discussion? Discussion { get; set; }
+        public Guid? DiscussionId { get; set; }
+    }
+}
