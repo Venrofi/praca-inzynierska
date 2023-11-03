df3dc5d4dac68bd09f2ddbbeffd50473c04d1d38
Kamil Plewnia <plewnia.k@e80group.com>
2023-11-03 16:10:13 +0100

    [BE] Add: add first test migration

---
+++ Backend/Backend/Migrations/20231103145754_test.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class test : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    EventId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cover = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.EventId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    GroupId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Open = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.GroupId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PremiereAlbums",
+                columns: table => new
+                {
+                    PremiereAlbumId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Artist = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cover = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReleaseDate = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PremiereAlbums", x => x.PremiereAlbumId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Avatar = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PremiereAlbumDetails",
+                columns: table => new
+                {
+                    PremiereAlbumDetailsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Duration = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Genre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rating = table.Column<double>(type: "float", nullable: false),
+                    PremiereAlbumId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PremiereAlbumDetails", x => x.PremiereAlbumDetailsId);
+                    table.ForeignKey(
+                        name: "FK_PremiereAlbumDetails_PremiereAlbums_PremiereAlbumId",
+                        column: x => x.PremiereAlbumId,
+                        principalTable: "PremiereAlbums",
+                        principalColumn: "PremiereAlbumId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DiscossionPosts",
+                columns: table => new
+                {
+                    DiscussionPostId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Topic = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    NumberOfComments = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DiscossionPosts", x => x.DiscussionPostId);
+                    table.ForeignKey(
+                        name: "FK_DiscossionPosts_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GroupUser",
+                columns: table => new
+                {
+                    GroupsGroupId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UsersUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GroupUser", x => new { x.GroupsGroupId, x.UsersUserId });
+                    table.ForeignKey(
+                        name: "FK_GroupUser_Groups_GroupsGroupId",
+                        column: x => x.GroupsGroupId,
+                        principalTable: "Groups",
+                        principalColumn: "GroupId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GroupUser_Users_UsersUserId",
+                        column: x => x.UsersUserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tracks",
+                columns: table => new
+                {
+                    TrackId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Duration = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PremiereAlbumDetailsId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tracks", x => x.TrackId);
+                    table.ForeignKey(
+                        name: "FK_Tracks_PremiereAlbumDetails_PremiereAlbumDetailsId",
+                        column: x => x.PremiereAlbumDetailsId,
+                        principalTable: "PremiereAlbumDetails",
+                        principalColumn: "PremiereAlbumDetailsId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DiscossionPostsDetails",
+                columns: table => new
+                {
+                    DiscussionPostDetailsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DiscussionPostId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DiscossionPostsDetails", x => x.DiscussionPostDetailsId);
+                    table.ForeignKey(
+                        name: "FK_DiscossionPostsDetails_DiscossionPosts_DiscussionPostId",
+                        column: x => x.DiscussionPostId,
+                        principalTable: "DiscossionPosts",
+                        principalColumn: "DiscussionPostId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    CommentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DiscussionPostDetailsId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.CommentId);
+                    table.ForeignKey(
+                        name: "FK_Comments_DiscossionPostsDetails_DiscussionPostDetailsId",
+                        column: x => x.DiscussionPostDetailsId,
+                        principalTable: "DiscossionPostsDetails",
+                        principalColumn: "DiscussionPostDetailsId");
+                    table.ForeignKey(
+                        name: "FK_Comments_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_DiscussionPostDetailsId",
+                table: "Comments",
+                column: "DiscussionPostDetailsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_UserId",
+                table: "Comments",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DiscossionPosts_UserId",
+                table: "DiscossionPosts",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DiscossionPostsDetails_DiscussionPostId",
+                table: "DiscossionPostsDetails",
+                column: "DiscussionPostId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupUser_UsersUserId",
+                table: "GroupUser",
+                column: "UsersUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PremiereAlbumDetails_PremiereAlbumId",
+                table: "PremiereAlbumDetails",
+                column: "PremiereAlbumId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tracks_PremiereAlbumDetailsId",
+                table: "Tracks",
+                column: "PremiereAlbumDetailsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "Events");
+
+            migrationBuilder.DropTable(
+                name: "GroupUser");
+
+            migrationBuilder.DropTable(
+                name: "Tracks");
+
+            migrationBuilder.DropTable(
+                name: "DiscossionPostsDetails");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+
+            migrationBuilder.DropTable(
+                name: "PremiereAlbumDetails");
+
+            migrationBuilder.DropTable(
+                name: "DiscossionPosts");
+
+            migrationBuilder.DropTable(
+                name: "PremiereAlbums");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
