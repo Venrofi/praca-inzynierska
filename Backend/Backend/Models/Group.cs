948e8820072ed899801c5a1aeb99dd3bdd1b35c4
Kamil Plewnia <plewnia.k@e80group.com>
2023-11-03 08:27:04 +0100

    [BE] Add: add groups

---
+++ Backend/Backend/Models/Group.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
+{
+    public class Group
+    {
+        [Required]
+        public Guid GroupId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public bool Open { get; set; }
+
+        public List<User> Users { get; set; }
+    }
+}
