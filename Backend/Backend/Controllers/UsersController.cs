19110df8956cf7f742fcc2f6895d3d163019ff81
Kamil Plewnia <plewnia.k@e80group.com>
2023-11-03 16:09:22 +0100

    [BE] Change: changes in UsersController

--- Backend/Backend/Controllers/UsersController.cs
+++ Backend/Backend/Controllers/UsersController.cs
@@ -5,8 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Backend.Models;
-using Backend.Context;
+using Backend.Core.Entities;
+using Backend.Data.Context;
 
 namespace Backend.Controllers
 {
