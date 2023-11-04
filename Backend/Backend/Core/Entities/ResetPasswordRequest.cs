aa2572c8f69764fbfa8aaa675296c57b1ec49802
kubicahere <kubica.here@gmail.com>
2023-11-04 21:53:55 +0100

    [BE] Added UserLogin, UserRegister and ResetPassword entities

--- Backend/Backend/Core/Entities/ResetPasswordRequest.cs
+++ Backend/Backend/Core/Entities/ResetPasswordRequest.cs
@@ -2,12 +2,10 @@
 
 namespace Backend.Core.Entities
 {
-    public class UserRegisterRequest
+    public class ResetPasswordRequest
     {
-        [Required, MinLength(4)]
-        public string Username { get; set; } = string.Empty;
-        [Required, EmailAddress]
-        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string Token { get; set; } = string.Empty;
         [Required, MinLength(8, ErrorMessage = "Minimum length for password is 8 characters!")]    //TODO WORK OUT PASSWORD REQUIREMENTS
         public string Password { get; set; } = string.Empty;
         [Required, Compare("Password")]
