ca02c2921b1ca65646834adcf3f8e1c9f831b7de
kubicahere <kubica.here@gmail.com>
2023-11-04 14:35:09 +0100

    [BE] Created POST register method & cryptography methods

--- Backend/Backend/Controllers/LoginRegisterController.cs
+++ Backend/Backend/Controllers/LoginRegisterController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Backend.Core.Entities;
+using Backend.Data.Context;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
 
 namespace Backend.Controllers
 {
@@ -8,8 +11,49 @@ namespace Backend.Controllers
     [ApiController]
     public class LoginRegisterController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
 
+        public LoginRegisterController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserRegisterRequest request)
+        {
+            if (_context.Users.Any(x => x.Email == request.Email))
+                return BadRequest("This e-mail is already in use!");
 
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            var user = new User
+            {
+                UserId = new Guid(),
+                UserName = request.Username,
+                Email = request.Email,
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt,
+                VerificationToken = CreateRandomToken()
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok("User created!");
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using(var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private string CreateRandomToken()
+        {
+            return Convert.ToHexString(RandomNumberGenerator.GetBytes(64));
+        }
     }
 }
