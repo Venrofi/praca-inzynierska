428231e023ebbfbbb3a18de2007ef8ab107c54e3
kubicahere <kubica.here@gmail.com>
2023-11-04 14:36:17 +0100

    [BE] Added entity for register request & adjusted user entity configuration

--- Backend/Backend/Core/Entities/User.cs
+++ Backend/Backend/Core/Entities/User.cs
@@ -7,25 +7,33 @@ namespace Backend.Core.Entities
 {
     public class User
     {
-        [Required]
+        //[Required]
         public Guid UserId { get; set; }
 
-        [Required]
-        public string UserName { get; set; }
-
-        [Required]
-        [DataType(DataType.Password)]
-        //[StringLength(15, ErrorMessage = "Your Password is limited to {2} to {1} characters", MinimumLength = 6)]
-        public string Password { get; set; }
-
-        [Required]
-        [DataType(DataType.EmailAddress)]
+       // [Required]
+        //[DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
-        [Required]
+        //[Required]
+        public string UserName { get; set; }
+
+        //[Required]
+        //[DataType(DataType.Password)]
+        //[StringLength(15, ErrorMessage = "Your Password is limited to {2} to {1} characters", MinimumLength = 6)]
+        // public string Password { get; set; }
+
+        public byte[] PasswordHash { get; set; } = new byte[32];
+        public byte[] PasswordSalt { get; set; } = new byte[32];
+        public string? VerificationToken { get; set; }
+        public DateTime? VerificationTime { get; set; }
+        public string? PasswordResetToken { get; set; }
+        public DateTime? ResetTokenExpiration { get; set; }
+
+
+        //[Required]
         public string Avatar { get; set; }
 
-        [Required]
+        //[Required]
         public Roles Role { get; set; } = Roles.USER;
 
         public enum Roles
---
+++ Backend/Backend/Core/Entities/UserRegisterRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Core.Entities
+{
+    public class UserRegisterRequest
+    {
+        [Required, MinLength(4)]
+        public string Username { get; set; } = string.Empty;
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required, MinLength(8)]    //TODO WORK OUT PASSWORD REQUIREMENTS
+        public string Password { get; set; } = string.Empty;
+        [Required, Compare("Password")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
