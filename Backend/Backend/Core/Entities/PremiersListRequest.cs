c39330d23889adfcf212192304bc23b1b4b9fef6
Kamil Plewnia <plewnia.k@e80group.com>
2023-11-07 09:51:35 +0100

    [BE] add: init premierslistrequest

---
+++ Backend/Backend/Core/Entities/PremiersListRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Core.Entities
+{
+    public class PremiersListRequest
+    {
+        [Required]
+        public string SearchFraze { get; set; } = string.Empty;
+    }
+}
