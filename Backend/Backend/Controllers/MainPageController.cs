111e964e53b46dd7f9b9d3dbd4e31e7c285a7b18
Kamil Plewnia <plewnia.k@e80group.com>
2023-11-07 09:49:31 +0100

    [BE] add: init mainpagecontroller

---
+++ Backend/Backend/Controllers/MainPageController.cs
@@ -0,0 +1,44 @@
+﻿using Backend.Core.Entities;
+using Backend.Data.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MainPageController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DiscussionPost>>> GetDiscussionsList()
+        {
+            if (_context.DiscossionPosts == null)
+            {
+                return NotFound();
+            }
+            return await _context.DiscossionPosts.OrderByDescending(dp => dp.CreationTime).ToListAsync();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Event>>> GetEventsList()
+        {
+            if (_context.Events == null)
+            {
+                return NotFound();
+            }
+            return await _context.Events.OrderByDescending(e=>e.Date).ToListAsync();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PremiereAlbum>>> GetPremiersList()
+        {
+            if (_context.PremiereAlbums == null)
+            {
+                return NotFound();
+            }
+            return await _context.PremiereAlbums.OrderByDescending(dp => dp.ReleaseDate).ToListAsync();
+        }
+    }
+}
