5fda2d41f2f925ac54d805780f4f13bfeb2034f8
Kamil Plewnia <plewnia.k@e80group.com>
2023-11-07 09:50:59 +0100

    [BE] add: init discussionslistrequest

---
+++ Backend/Backend/Core/Entities/DiscussionsListRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Core.Entities
+{
+    public class DiscussionsListRequest
+    {
+        [Required]
+        public string SearchFraze { get; set; } = string.Empty;
+    }
+}
