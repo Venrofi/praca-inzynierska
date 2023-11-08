09295815119defb8fe3bbf0853978de1344c9285
Kamil Plewnia <plewnia.k@e80group.com>
2023-11-08 16:33:29 +0100

    [BE] change: changes in user, remove nullable email and username

--- Backend/Backend/Core/Entities/User.cs
+++ Backend/Backend/Core/Entities/User.cs
@@ -8,8 +8,8 @@ namespace Backend.Core.Entities
     public class User
     {
         public Guid UserId { get; set; }
-        public string? Email { get; set; }
-        public string? UserName { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
         public string? Avatar { get; set; }
 
         public byte[] PasswordHash { get; set; } = new byte[32];
