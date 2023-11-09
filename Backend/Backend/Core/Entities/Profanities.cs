027f07b19f03e58cac8391a02f92818e0876969a
Kamil Plewnia <plewnia.k@e80group.com>
2023-11-09 14:39:39 +0100

    [BE] add: add profanities/vulgarisms

---
+++ Backend/Backend/Core/Entities/Profanities.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Core.Entities {
+    public class Profanities {
+        [Required]
+        public Guid ProfanitiesId { get; set; }
+
+        [Required]
+        public string ProfanitiesName { get; set; }
+    }
+}
