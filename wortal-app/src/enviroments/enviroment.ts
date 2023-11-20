dfde0ddda9f49961bc14691b1cfd1bfddb35789f
Venrofi <radoslaw.terelak@gmail.com>
2023-11-20 19:20:40 +0100

    [FE] Use Azure hosted API

--- wortal-app/src/enviroments/enviroment.ts
+++ wortal-app/src/enviroments/enviroment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://localhost:7145/api', // API base URL for local development
+  apiBaseUrl: 'https://backend-hip-hop-hub.azurewebsites.net/api', // API base URL for Azure BE
+  // apiBaseUrl: 'https://localhost:7145/api', // API base URL for local development
 };
