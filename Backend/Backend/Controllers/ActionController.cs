812404c006868e1156492e44ab86eb0116197b72
Kamil Plewnia <plewnia.k@e80group.com>
2023-11-22 08:12:25 +1100

    [BE] change: changes in actioncontroller, add functionalities

--- Backend/Backend/Controllers/ActionController.cs
+++ Backend/Backend/Controllers/ActionController.cs
@@ -1,6 +1,7 @@
 ﻿using Backend.Core.Entities;
 using Backend.Data.Context;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Controllers {
     public class ActionController :ControllerBase{
@@ -10,28 +11,83 @@ namespace Backend.Controllers {
         public ActionController(ApplicationDbContext context) {
             _context = context;
         }
+        //Warto dodać obsługę błędów (np. wrong id, can’t follow/join/attend) i zwrócenie odpowiedniego kodu błędu w celu prezentacji błędu w UI.
 
         #region Follow
-        [HttpPost("follow{artist_id}&{user_id}")]
+        [HttpPost("follow")]
         public async Task<IActionResult> FollowArtist(Guid artistId, Guid userId) {
+            var artist = await _context.ArtistsProfiles.Include(a => a.Followers).Where(a => a.ArtistProfileId == artistId).FirstOrDefaultAsync();
+            var user = await _context.Users.Include(u => u.FollowedArtists).Where(u => u.UserId == userId).FirstOrDefaultAsync();
 
-            return Ok($"New group was created.");
+            if (user == null)
+                return BadRequest(new { code = "wrong-id" });
+            if (artist == null)
+                return BadRequest(new { code = "wrong-id" });
+            //check if included lists are nulles
+            artist.Followers ??= new List<User>();
+            user.FollowedArtists ??= new List<ArtistProfile>();
+            //check if group already has user in users and vice versa
+            if (artist.Followers.Contains(user) || user.FollowedArtists.Contains(artist))
+                return BadRequest(new { code = "cant-follow" });
+
+            artist.Followers.Add(user);
+            //await _context.SaveChangesAsync();
+
+            user.FollowedArtists.Add(artist);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { code = "success" });
         }
         #endregion
 
         #region Join
-        [HttpPost("join{group_id}&{user_id}")]
+        [HttpPost("join")]
         public async Task<IActionResult> JoinGroup(Guid groupId, Guid userId) {
+            var group = await _context.Groups.Include(g => g.Users).Where(g => g.GroupId == groupId).FirstOrDefaultAsync();
+            var user = await _context.Users.Include(u => u.Groups).Where(u => u.UserId == userId).FirstOrDefaultAsync();
 
-            return Ok($"New group was created.");
+            if (user == null)
+                return BadRequest(new { code = "wrong-id" });
+            if (group == null)
+                return BadRequest(new { code = "wrong-id" });
+            //check if included lists are nulles
+            group.Users ??= new List<User>();
+            user.Groups ??= new List<Group>();
+            if (group.Users.Contains(user) || user.Groups.Contains(group))
+                return BadRequest(new { code = "cant-join" });
+
+            group.Users.Add(user);
+            //await _context.SaveChangesAsync();
+
+            user.Groups.Add(group);
+            await _context.SaveChangesAsync();
+            return Ok(new { code = "success" });
         }
         #endregion
 
         #region Attend
-        [HttpPost("attend{event_id}&{user_id}")]
-        public async Task<IActionResult> AttendEvent(Guid EventId, Guid userId) {
+        [HttpPost("attend")]
+        public async Task<IActionResult> AttendEvent(Guid eventId, Guid userId) {
+            var eventt = await _context.Events.Include(e => e.Participants).Where(e => e.EventId == eventId).FirstOrDefaultAsync();
+            var user = await _context.Users.Include(u => u.ParticipatedEvents).Where(u => u.UserId == userId).FirstOrDefaultAsync();
 
-            return Ok($"New group was created.");
+            if (user == null)
+                return BadRequest(new { code = "wrong-id" });
+            if (eventt == null)
+                return BadRequest(new { code = "wrong-id" });
+            //check if included lists are nulles
+            eventt.Participants ??= new List<User>();
+            user.ParticipatedEvents ??= new List<Event>();
+            //check if event already has user in participants and vice versa
+            if (eventt.Participants.Contains(user) || user.ParticipatedEvents.Contains(eventt))
+                return BadRequest(new { code = "cant-attend" });
+
+            eventt.Participants.Add(user);
+            //await _context.SaveChangesAsync();
+
+            user.ParticipatedEvents.Add(eventt);
+            await _context.SaveChangesAsync();
+            return Ok(new { code = "success" });
         }
         #endregion
     }
