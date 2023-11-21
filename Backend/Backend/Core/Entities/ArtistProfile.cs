4e74ad80397e5f8dfa95562197eec728d0d8aee9
Kamil Plewnia <plewnia.k@e80group.com>
2023-11-22 08:14:36 +1100

    [BE] change: changes in artistprofile entity, add image

--- Backend/Backend/Core/Entities/ArtistProfile.cs
+++ Backend/Backend/Core/Entities/ArtistProfile.cs
@@ -6,6 +6,7 @@
 
         public string Name { get; set; }
         public string? Description { get; set; }
+        public string? Image { get; set; }
 
         //public DiscussionPost DiscussionPost { get; set; }
         public List<PremiereAlbum>? Albums { get; set; } 
