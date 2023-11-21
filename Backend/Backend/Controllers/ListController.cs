b65ba0022ef04e793b4b63c5286f645df1002599
Kamil Plewnia <plewnia.k@e80group.com>
2023-11-22 08:13:02 +1100

    [BE] change: changes in listcontroller, add functionalities

--- Backend/Backend/Controllers/ListController.cs
+++ Backend/Backend/Controllers/ListController.cs
@@ -13,93 +13,30 @@ namespace Backend.Controllers {
         #region Artists
         [HttpGet("artists")]
         public async Task<ActionResult<object>> GetArtists() {
-            //todo
-            //id, username, email, groups, role, posts, avatar
-            if (_context.Users == null) {
+            if (_context.ArtistsProfiles == null) {
                 return NotFound();
             }
-            Guid id = Guid.NewGuid();
-            var resUser = await _context.Users.Include(u => u.Groups).Include(u => u.DiscussionPosts).Include(u => u.UserType).Include(u => u.ParticipatedEvents).Include(u => u.FollowedArtists).Where(u => u.UserId == id).FirstOrDefaultAsync();
-
-            if (resUser == null) {
-                return NotFound();
-            }
-
-            var res = new {
-                Id = resUser.UserId,
-                Name = resUser.UserName,
-                Avatar = resUser.Avatar,
-                Bio = "", // TODO: Add Bio field!
-                Email = resUser.Email,
-                Posts = resUser.DiscussionPosts.Select(dp => new { id = dp.DiscussionPostId, name = dp.Title }),
-                JoinedGroups = resUser.Groups.Select(g => new { id = g.GroupId, name = g.Name }),
-                AttendedEvents = resUser.ParticipatedEvents.Select(pe => new { id = pe.EventId, name = pe.Title }),
-                FollowedArtists = resUser.FollowedArtists.Select(fa => new { id = fa.ArtistProfileId, name = fa.Name }),
-                Role = resUser.UserType.Description,
-            };
-            return res;
+            return await _context.ArtistsProfiles.Select(a => new { id = a.ArtistProfileId, name = a.Name, image = a.Image }).ToListAsync();
         }
         #endregion
 
         #region Groups
         [HttpGet("groups")]
         public async Task<ActionResult<object>> GetGroups() {
-            //todo
-            //id, username, email, groups, role, posts, avatar
-            if (_context.Users == null) {
+            if (_context.Groups == null) {
                 return NotFound();
             }
-            Guid id = Guid.NewGuid();
-            var resUser = await _context.Users.Include(u => u.Groups).Include(u => u.DiscussionPosts).Include(u => u.UserType).Include(u => u.ParticipatedEvents).Include(u => u.FollowedArtists).Where(u => u.UserId == id).FirstOrDefaultAsync();
-
-            if (resUser == null) {
-                return NotFound();
-            }
-
-            var res = new {
-                Id = resUser.UserId,
-                Name = resUser.UserName,
-                Avatar = resUser.Avatar,
-                Bio = "", // TODO: Add Bio field!
-                Email = resUser.Email,
-                Posts = resUser.DiscussionPosts.Select(dp => new { id = dp.DiscussionPostId, name = dp.Title }),
-                JoinedGroups = resUser.Groups.Select(g => new { id = g.GroupId, name = g.Name }),
-                AttendedEvents = resUser.ParticipatedEvents.Select(pe => new { id = pe.EventId, name = pe.Title }),
-                FollowedArtists = resUser.FollowedArtists.Select(fa => new { id = fa.ArtistProfileId, name = fa.Name }),
-                Role = resUser.UserType.Description,
-            };
-            return res;
+            return await _context.Groups.Select(g => new { id = g.GroupId, name = g.Name, image = g.Image }).ToListAsync();
         }
         #endregion
 
         #region Members
         [HttpGet("members")]
         public async Task<ActionResult<object>> GetMembers() {
-            //todo
-            //id, username, email, groups, role, posts, avatar
             if (_context.Users == null) {
                 return NotFound();
             }
-            Guid id = Guid.NewGuid();
-            var resUser = await _context.Users.Include(u => u.Groups).Include(u => u.DiscussionPosts).Include(u => u.UserType).Include(u => u.ParticipatedEvents).Include(u => u.FollowedArtists).Where(u => u.UserId == id).FirstOrDefaultAsync();
-
-            if (resUser == null) {
-                return NotFound();
-            }
-
-            var res = new {
-                Id = resUser.UserId,
-                Name = resUser.UserName,
-                Avatar = resUser.Avatar,
-                Bio = "", // TODO: Add Bio field!
-                Email = resUser.Email,
-                Posts = resUser.DiscussionPosts.Select(dp => new { id = dp.DiscussionPostId, name = dp.Title }),
-                JoinedGroups = resUser.Groups.Select(g => new { id = g.GroupId, name = g.Name }),
-                AttendedEvents = resUser.ParticipatedEvents.Select(pe => new { id = pe.EventId, name = pe.Title }),
-                FollowedArtists = resUser.FollowedArtists.Select(fa => new { id = fa.ArtistProfileId, name = fa.Name }),
-                Role = resUser.UserType.Description,
-            };
-            return res;
+            return await _context.Users.Select(u => new { id = u.UserId, name = u.UserName, avatar = u.Avatar}).ToListAsync();
         }
         #endregion
     }
