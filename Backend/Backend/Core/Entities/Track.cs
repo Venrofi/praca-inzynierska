6a4108a9fe2751500bea8d899d503a51f1b6a8de
Kamil Plewnia <plewnia.k@e80group.com>
2023-11-22 08:15:47 +1100

    [BE] change: changes in track entity, change duration to timespan

--- Backend/Backend/Core/Entities/Track.cs
+++ Backend/Backend/Core/Entities/Track.cs
@@ -4,14 +4,9 @@ namespace Backend.Core.Entities
 {
     public class Track
     {
-        [Required]
         public Guid TrackId { get; set; }
-
-        [Required]
         public string Title { get; set; }
-
-        [Required]
-        public string Duration { get; set; }
+        public TimeSpan Duration { get; set; }
 
         public PremiereAlbumDetails PremiereAlbumDetails { get; set;}
         public Guid PremiereAlbumDetailsId { get; set; }
