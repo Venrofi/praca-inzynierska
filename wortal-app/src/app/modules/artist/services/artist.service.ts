6713bc2b71db84919f1673e7e354ccf362e22c71
Venrofi <radoslaw.terelak@gmail.com>
2023-11-22 19:32:47 +0100

    [FE] Final ArtistsList implementation

--- wortal-app/src/app/modules/artist/services/artist.service.ts
+++ wortal-app/src/app/modules/artist/services/artist.service.ts
@@ -2,26 +2,39 @@ import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from '@angular/core';
 import { map, Observable } from "rxjs";
 import { environment } from "../../../../enviroments/enviroment";
-import { Artist } from "../../../core/core.model";
+import { Artist, ArtistList } from "../../../core/core.model";
 
 @Injectable()
 export class ArtistService {
   private API_ROOT = environment.apiBaseUrl;
   constructor(private http: HttpClient) { }
 
-  getArtistsList(): Observable<Artist[]> {
+  getArtistsList(): Observable<ArtistList[]> {
     // return this.http.get<Artist[]>(`${this.API_ROOT}/List/artists`);
-    return this.http.get<Artist[]>('https://backend-hip-hop-hub.azurewebsites.net/artists'); // TODO: Wrong API endpoint address!
+    return this.http.get<ArtistList[]>('https://backend-hip-hop-hub.azurewebsites.net/artists')
+      .pipe(
+        map((artists: ArtistList[]) => {
+          return artists.map((artist: ArtistList, index: number) => {
+            return {
+              ...artist,
+              image: this.generateRandomAvatar(),
+              rank: (index + 1).toString(),
+            };
+          });
+        })
+      ); // TODO: Wrong API endpoint address!
   }
 
   getArtistInformation(artistID: string) {
     const params = new HttpParams().set('artistId', artistID);
 
     // return this.http.get<Artist>(`${this.API_ROOT}/Details/artist`, { params });
-    return this.http.get<Artist[]>('assets/data/artists.json').pipe(
-      map((artists: Artist[]) => {
-        return artists.find((artist: Artist) => artist.id === artistID);
-      })
-    );
+    return this.http.get<Artist[]>('assets/data/artists.json').pipe(map(artists => artists.find(artist => artist.id === '1')));
+  }
+
+  private generateRandomAvatar(): string {
+    const randomAvatarSize = Math.floor(Math.random() * 300 + 200); // returns a random number between 200 and 400
+
+    return `https://picsum.photos/${randomAvatarSize}/${randomAvatarSize}`;
   }
 }
