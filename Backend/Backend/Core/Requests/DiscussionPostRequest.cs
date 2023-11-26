ae0c185fe3e89adbf2087460f436f0af19c27e47
Kamil Plewnia <plewnia.k@e80group.com>
2023-11-27 09:26:54 +1100

    [BE] add/delete: DPrequest

---
+++ Backend/Backend/Core/Requests/DiscussionPostRequest.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Core.Requests {
+    public class DiscussionPostRequest {
+
+        [Required(ErrorMessage = "null-authorid")]
+        public Guid AuthorId { get; set; }
+
+        [Required(ErrorMessage = "null-groupid")]
+        public Guid GroupId { get; set; }
+
+        [Required(ErrorMessage = "null-title")]
+        [MaxLength(100, ErrorMessage = "long-title")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "null-content")]
+        [MaxLength(2000, ErrorMessage = "long-content")]
+        public string Content { get; set; } = string.Empty;
+
+    }
+}
