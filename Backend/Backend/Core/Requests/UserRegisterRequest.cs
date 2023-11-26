fb0f5108d03f87a8618d12842cd7481ed040aaca
Kamil Plewnia <plewnia.k@e80group.com>
2023-11-27 09:46:23 +1100

    [BE] change: register request, add regex to login

--- Backend/Backend/Core/Requests/UserRegisterRequest.cs
+++ Backend/Backend/Core/Requests/UserRegisterRequest.cs
@@ -5,11 +5,15 @@ namespace Backend.Core.Requests
     public class UserRegisterRequest
     {
         [Required, MinLength(4)]
+        [RegularExpression("^[a-zA-Z0-9.]+$", ErrorMessage = "username-error")]
         public string Username { get; set; } = string.Empty;
+
         [Required, EmailAddress]
         public string Email { get; set; } = string.Empty;
+
         [Required, MinLength(8, ErrorMessage = "Minimum length for password is 8 characters!")]    //TODO WORK OUT PASSWORD REQUIREMENTS
         public string Password { get; set; } = string.Empty;
+
         [Required, Compare("Password")]
         public string ConfirmPassword { get; set; } = string.Empty;
     }
