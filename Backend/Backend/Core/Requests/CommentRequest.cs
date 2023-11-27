4c3b513a87f0b0836b9bd670a93a6f0c08ec6253
Kamil Plewnia <plewnia.k@e80group.com>
2023-11-27 12:37:24 +1100

    [BE] add: add commentrequest

---
+++ Backend/Backend/Core/Requests/CommentRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Core.Requests {
+    public class CommentRequest {
+        [Required(ErrorMessage = "null-authorid")]
+        public Guid AuthorId { get; set; }
+
+        [Required(ErrorMessage = "null-discussionpostid")]
+        public Guid DiscussionPostId { get; set; }
+
+        [Required(ErrorMessage = "null-content")]
+        [MaxLength(2000, ErrorMessage = "long-content")]
+        public string Content { get; set; } = string.Empty;
+    }
+}
