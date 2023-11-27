67cdc1e2ce5b1218b9862bb9329fc11c4f465202
Kamil Plewnia <plewnia.k@e80group.com>
2023-11-28 10:29:18 +1100

    [BE] add: search algorithm

---
+++ Backend/Backend/Controllers/SearchController.cs
@@ -0,0 +1,86 @@
+﻿using Backend.Core.Entities;
+using Backend.Data.Context;
+using Backend.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase {
+        private readonly ApplicationDbContext _context;
+        private readonly ProfanitySearchAlgorithm _psa;
+
+        public SearchController(ApplicationDbContext context) {
+            _context = context;
+            _psa = new ProfanitySearchAlgorithm(_context);
+        }
+
+        #region Search
+        [HttpGet("search")]
+        public async Task<ActionResult<object>> Search(Guid? id, string input) {
+
+            if (_context.DiscussionPosts == null || _context.ArtistsProfiles == null || _context.Users == null || _context.Groups == null || _context.Events == null) {
+                return NotFound(new { code = "entity-error"});
+            }
+            if (input == null || input == string.Empty || input.Length < 3) return BadRequest(new { code = "input-error"});
+
+            User? user = null;
+            if (id.HasValue) {
+                user = await _context.Users.Where(u => u.UserId == id).FirstOrDefaultAsync();
+                if (user == null)
+                    return NotFound(new { code = "user-not-found" });
+            }
+
+            //done
+            var artists = await _context.ArtistsProfiles
+                .Where(ap => ap.Name.Contains(input) || ap.Description.Contains(input))
+                .OrderBy(a => a.Name)
+                .Select(ap => new { id = ap.ArtistProfileId, name = ap.Name })
+                .ToArrayAsync();
+
+            //done
+            var events = await _context.Events
+                //.Include(e => e.Group)
+                .Include(e => e.Group.Users)
+                .Where(e => id.HasValue ? (e.Promotor == Event.PromotorType.Artist || (e.Promotor == Event.PromotorType.Group && e.Group.Users.Contains(user))) : (e.Promotor == Event.PromotorType.Artist))
+                //.Where(e => id.HasValue ? (e.Promotor == Event.PromotorType.Artist || (e.Promotor == Event.PromotorType.Group)) : (e.Promotor == Event.PromotorType.Artist))
+                .Where(e => id.HasValue ? (e.Group != null || e.ArtistProfile != null) : (e.ArtistProfile != null))
+                .Where(e => e.Title.Contains(input) || e.Description.Contains(input))
+                .OrderBy(e => e.Title)
+                .ThenBy(e => e.Date)
+                .Select(e => new { id = e.EventId, name = e.Title})
+                //.Select(e => new { id = e.EventId, name = e.Title, group = e.GroupId.HasValue ? (e.GroupId):(null) })
+                .ToArrayAsync();
+
+            //done
+            var posts = await _context.DiscussionPosts
+                .Include(dp => dp.DiscussionPostDetails)
+                .Include(dp => dp.Group.Users)
+                .Where(dp => id.HasValue ? (dp.TopicType == DiscussionPost.TopicTypes.Artist || (dp.TopicType == DiscussionPost.TopicTypes.Group && dp.Group.Users.Contains(user))) : (dp.TopicType == DiscussionPost.TopicTypes.Artist))
+                .Where(dp => id.HasValue ? (dp.Group != null || dp.ArtistProfile != null) : (dp.ArtistProfile != null))
+                .Where(dp => dp.Title.Contains(input) || dp.Topic.Contains(input) || dp.DiscussionPostDetails.Content.Contains(input))
+                .OrderBy(dp => dp.CreationTime)
+                .Select(dp => new { id = dp.DiscussionPostId, name = dp.Title })
+                //.Select(dp => new { id = dp.DiscussionPostId, name = dp.Title, group = dp.GroupId.HasValue ? (dp.GroupId):(null) })
+                .ToListAsync();
+
+            //done
+            var users = id.HasValue ? (await _context.Users
+                .Where(u => u.UserName.Contains(input))
+                .OrderBy(u => u.UserName)
+                .Select(u => new { id = u.UserId, name = u.UserName })
+                .ToListAsync()) : (null);
+
+            //done
+            var groups = id.HasValue ? (await _context.Groups
+                .Where(g => g.Name.Contains(input))
+                .OrderBy(g => g.Name)
+                .Select(gr => new { id = gr.GroupId, name = gr.Name })
+                .ToListAsync()) : (null);
+
+            return new { artists, events, posts, users, groups };
+        }
+        #endregion
+    }
+}
