a512ee9f964c4e17398d351b8f6fd6e2bec691f5
Venrofi <radoslaw.terelak@gmail.com>
2023-11-28 21:33:36 +0100

    [FE] DiscussionPostDetails Implementation (no add comment action)

--- wortal-app/src/app/shared/components/discussion-post-details/discussion-post-details.component.ts
+++ wortal-app/src/app/shared/components/discussion-post-details/discussion-post-details.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subject, debounceTime, switchMap } from 'rxjs';
+import { DiscussionPostDetails } from 'src/app/modules/homepage/homepage.model';
+import { ContentDetailsService } from '../../services/content-details.service';
+import { StoreModel } from 'src/app/app-state.model';
+import { Store } from '@ngrx/store';
+import { Member } from 'src/app/core/core.model';
 
 @Component({
   selector: 'app-discussion-post-details',
   templateUrl: './discussion-post-details.component.html',
-  styleUrls: ['./discussion-post-details.component.scss']
 })
-export class DiscussionPostDetailsComponent {
+export class DiscussionPostDetailsComponent implements OnInit {
+  discussionPost!: DiscussionPostDetails;
+
+  member: Member | undefined;
+
+  private clickSubject = new Subject<void>();
+
+  constructor(
+    private contentDetailsService: ContentDetailsService,
+    private store: Store<StoreModel>,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.store.select(state => state.app.member).subscribe(member => this.member = member);
+
+    this.route.queryParams.pipe(
+      switchMap(params => {
+        return this.contentDetailsService.getDiscussionPostDetails(params['id'])
+      })
+    ).subscribe(discussionPost => {
+      this.discussionPost = discussionPost;
+      console.log(this.discussionPost);
+    });
+
+    this.clickSubject.pipe(debounceTime(500)).subscribe(() => this.addComment());
+  }
+
+  commentAction() {
+    this.clickSubject.next();
+  }
+
+  addComment() {
+    console.log('Add comment');
+  }
 
 }
