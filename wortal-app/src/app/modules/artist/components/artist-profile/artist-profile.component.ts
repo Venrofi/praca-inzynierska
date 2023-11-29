d8d688facdca18266e4328c9c3664f817895238b
Venrofi <radoslaw.terelak@gmail.com>
2023-11-29 17:58:27 +0100

    [FE] ArtistProfile - Add unfollow action

--- wortal-app/src/app/modules/artist/components/artist-profile/artist-profile.component.ts
+++ wortal-app/src/app/modules/artist/components/artist-profile/artist-profile.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { switchMap } from 'rxjs';
+import { Subject, debounceTime, switchMap } from 'rxjs';
 import { StoreModel } from 'src/app/app-state.model';
 import { Artist, Member } from 'src/app/core/core.model';
 import * as memberActions from '../../../../store/member/member.actions';
@@ -18,6 +18,8 @@ export class ArtistProfileComponent implements OnInit {
   artistFollowed: boolean = false;
 
   member: Member | undefined;
+
+  private clickSubject = new Subject<void>();
 
   constructor(
     private artistService: ArtistService,
@@ -37,6 +39,10 @@ export class ArtistProfileComponent implements OnInit {
       this.artist = artist;
       this.artist.followers.find(follower => follower.id === this.member?.id) ? this.artistFollowed = true : this.artistFollowed = false;
     });
+
+    this.clickSubject.pipe(debounceTime(500)).subscribe(() => {
+      this.artistFollowed ? this.unfollowArtist() : this.followArtist();
+    });
   }
 
   openAlbumModal(albumId: string) {
@@ -44,8 +50,8 @@ export class ArtistProfileComponent implements OnInit {
     // TODO: Add Album Modal Component
   }
 
-  followArtist(artistId: string) {
-    this.artistService.followArtist(artistId).subscribe({
+  followArtist() {
+    this.artistService.followArtist(this.artist.id).subscribe({
       next: () => {
         if (this.member) {
           this.artistFollowed = true;
@@ -54,10 +60,10 @@ export class ArtistProfileComponent implements OnInit {
           this.artist.followers = [...this.artist.followers, { id: this.member.id, name: this.member.name }];
 
           // Update global store state
-          const followedArtists = [...this.member.joinedGroups, { id: this.artist.id, name: this.artist.name }];
+          const followedArtists = [...this.member.followedArtists, { id: this.artist.id, name: this.artist.name }];
           this.store.dispatch(memberActions.update({ member: { ...this.member, followedArtists } }));
 
-          this.snackBar.open('Zaobserwowałeś tego artystę!', 'OK', {
+          this.snackBar.open('Zaobserwowałeś tego Artystę.', 'OK', {
             duration: 3000,
             horizontalPosition: 'end',
             panelClass: ['snackbar-success']
@@ -65,12 +71,46 @@ export class ArtistProfileComponent implements OnInit {
         }
       },
       error: () => {
-        this.snackBar.open('Wystąpił błąd podczas próby zaobserwowania tego artysty!', 'OK', {
+        this.snackBar.open('Wystąpił błąd podczas próby zaobserwowania tego Artysty!', 'OK', {
           duration: 3000,
           horizontalPosition: 'end',
           panelClass: ['snackbar-error']
         });
       }
     });
+  }
+
+  unfollowArtist() {
+    this.artistService.unfollowArtist(this.artist.id).subscribe({
+      next: () => {
+        if (this.member) {
+          this.artistFollowed = false;
+
+          // Update local state
+          this.artist.followers = this.artist.followers.filter(follower => follower.id !== this.member?.id);
+
+          // Update global store state
+          const followedArtists = this.member.followedArtists.filter(artist => artist.id !== this.artist?.id);
+          this.store.dispatch(memberActions.update({ member: { ...this.member, followedArtists } }));
+
+          this.snackBar.open('Zrezygnowałeś z obserwacji tego Artysty.', 'OK', {
+            duration: 3000,
+            horizontalPosition: 'end',
+            panelClass: ['snackbar-success']
+          });
+        }
+      },
+      error: () => {
+        this.snackBar.open('Nie udało się zrezygnować z obserwacji tego Artysty!', 'OK', {
+          duration: 3000,
+          horizontalPosition: 'end',
+          panelClass: ['snackbar-error']
+        });
+      }
+    });
+  }
+
+  artistAction() {
+    this.clickSubject.next();
   }
 }
