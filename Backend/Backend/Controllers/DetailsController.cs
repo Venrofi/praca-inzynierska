5e4ca26b0db6bbd7d215df84de4bb226fdc710ab
Kamil Plewnia <plewnia.k@e80group.com>
2023-11-29 13:58:32 +1100

    [BE] change: changes in details, simplify code, add artist BWE in album details

--- Backend/Backend/Controllers/DetailsController.cs
+++ Backend/Backend/Controllers/DetailsController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 using Microsoft.IdentityModel.Tokens;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection.Metadata.Ecma335;
 using System.Runtime.Intrinsics.Arm;
 
 namespace Backend.Controllers {
@@ -20,34 +22,26 @@ namespace Backend.Controllers {
 
         [HttpGet("artist")]
         public async Task<ActionResult<object>> GetArtistDetails(Guid id) {
+            if (_context.ArtistsProfiles == null) return NotFound(new { code = "entity-error"});
 
-            if (_context.ArtistsProfiles == null) {
-                return NotFound();
-            }
-
-            var resArtist = await _context.ArtistsProfiles
+            var artist = await _context.ArtistsProfiles
                 .Include(a => a.OrganizedEvents)
                 .Include(a => a.DiscussionPosts)
                 .Include(a => a.Followers)
                 .Include(a => a.Albums)
-                .Where(a => a.ArtistProfileId == id).FirstOrDefaultAsync();
+                .Where(a => a.ArtistProfileId == id)
+                .Select( a => new {
+                    id = a.ArtistProfileId,
+                    name = a.Name,
+                    image = a.Image,
+                    description = a.Description,
+                    albums = a.Albums.Select(a => new { id = a.PremiereAlbumId, name = a.Title, cover = a.Cover, releaseDate = a.ReleaseDate }),
+                    events = a.OrganizedEvents.Select(e => new { id = e.EventId, name = e.Title }),
+                    followers = a.Followers.Select(f => new { id = f.UserId, name = f.UserName }),
+                    discussionPosts = a.DiscussionPosts.Select(d => new { id = d.DiscussionPostId, name = d.Title })
+                }).FirstOrDefaultAsync();
 
-            if (resArtist == null) {
-                return NotFound();
-            }
-
-            //jesli cos niepotrzebne -> zakomentowac
-            var res = new {
-                id = resArtist.ArtistProfileId,
-                name = resArtist.Name,
-                image = resArtist.Image,
-                description = resArtist.Description,
-                albums = resArtist.Albums.Select(a => new { id = a.PremiereAlbumId, name = a.Title, cover = a.Cover, releaseDate = a.ReleaseDate }),
-                events = resArtist.OrganizedEvents.Select(e => new { id = e.EventId, name = e.Title }),
-                followers = resArtist.Followers.Select(f => new { id = f.UserId, name = f.UserName }),
-                discussionPosts = resArtist.DiscussionPosts.Select(d => new { id = d.DiscussionPostId, name = d.Title })
-            };
-            return res;
+            return artist == null ? (BadRequest(new { code = "artist-not-found"})) : (artist);
         }
 
         #endregion
@@ -55,175 +49,142 @@ namespace Backend.Controllers {
         #region Group
         [HttpGet("group")]
         public async Task<ActionResult<object>> GetGroupDetails(Guid id) {
-            /*// GROUP
-                export interface Group extends BaseWortalElement {
-                  image: string;
-                  description: string;
-                  events: BaseWortalElement[]; // Events created by the Group
-                  members: BaseWortalElement[]; // Members that joined the Group
-                  discussionPosts: BaseWortalElement[]; // Discussion posts created by the Group
-                }*/
-            if (_context.Groups == null) {
-                return NotFound();
-            }
+            if (_context.Groups == null) return NotFound(new { code = "entity-error" });
 
-            var group = await _context.Groups.Include(g => g.OrganizedEvents).Include(g => g.Users).Include(g => g.DiscussionPosts).Where(g => g.GroupId == id).FirstOrDefaultAsync();
-
-            if (group == null) {
-                return NotFound();
-            }
-
-            var res = new {
-                id = group.GroupId,
-                name = group.Name,
-                image = group.Image,
-                description = group.Description,
-                events = group.OrganizedEvents.Select(e => new {
-                    id = e.EventId,
-                    name = e.Title
-                }),
-                members = group.Users.Select(u => new {
-                    id = u.UserId,
-                    name = u.UserName
-                }),
-                discussionPosts = group.DiscussionPosts.Select(d => new {
-                    id = d.DiscussionPostId,
-                    name = d.Title
+            var group = await _context.Groups
+                .Include(g => g.OrganizedEvents)
+                .Include(g => g.Users)
+                .Include(g => g.DiscussionPosts)
+                .Where(g => g.GroupId == id)
+                .Select(g => new {
+                    id = g.GroupId,
+                    name = g.Name,
+                    image = g.Image,
+                    description = g.Description,
+                    events = g.OrganizedEvents.Select(e => new {
+                        id = e.EventId,
+                        name = e.Title
+                    }),
+                    members = g.Users.Select(u => new {
+                        id = u.UserId,
+                        name = u.UserName
+                    }),
+                    discussionPosts = g.DiscussionPosts.Select(d => new {
+                        id = d.DiscussionPostId,
+                        name = d.Title
+                    })
                 })
-            };
-            return res;
+                .FirstOrDefaultAsync();
+
+            return group == null ? (BadRequest(new { code = "group-not-found" })) :(group);
         }
         #endregion
 
         #region Event
         [HttpGet("event")]
         public async Task<ActionResult<object>> GetEventDetails(Guid id) {
-            if (_context.Events == null) {
-                return NotFound();
-            }
+            if (_context.Events == null) return NotFound(new { code = "entity-error" });
 
-            var eventt = await _context.Events
+            var eve = await _context.Events
                 .Include(e => e.Participants)
                 .Include(e => e.Group)
                 .Include(e => e.ArtistProfile)
-                .Where(e => e.EventId == id).FirstOrDefaultAsync();
-            if (eventt == null) {
-                return NotFound();
-            }
-
-            var res = new {
-                id = eventt.EventId,
-                name = eventt.Title,
-                date = eventt.Date,
-                image = eventt.Cover,
-                location = eventt.Location,
-                description = eventt.Description,
-                promoter = new {
-                    id = eventt.ArtistProfileId.HasValue ? eventt.ArtistProfileId : eventt.GroupId,
-                    name = eventt.ArtistProfile != null ? eventt.ArtistProfile.Name : eventt.Group.Name
-                },
-                participants = eventt.Participants.Select(p => new {
-                    id = p.UserId,
-                    name = p.UserName
+                .Where(e => e.EventId == id)
+                .Select(e => new {
+                    id = e.EventId,
+                    name = e.Title,
+                    date = e.Date,
+                    image = e.Cover,
+                    location = e.Location,
+                    description = e.Description,
+                    promoter = new {
+                        id = e.ArtistProfileId.HasValue ? e.ArtistProfileId : e.GroupId,
+                        name = e.ArtistProfile != null ? e.ArtistProfile.Name : e.Group.Name
+                    },
+                    participants = e.Participants.Select(p => new {
+                        id = p.UserId,
+                        name = p.UserName
+                    })
                 })
-            };
-            return res;
+                .FirstOrDefaultAsync();
+
+            return eve == null ? (BadRequest(new { code = "event-not-found" })) : (eve);
         }
         #endregion
 
         #region Discussion
         [HttpGet("discussion")]
         public async Task<ActionResult<object>> GetDiscussionDetails(Guid id) {
-            if (_context.DiscussionPosts == null || _context.DiscussionPostsDetails == null) {
-                return NotFound();
-            }
+            if (_context.DiscussionPosts == null || _context.DiscussionPostsDetails == null) return NotFound(new { code = "entity-error" });
 
             var post = await _context.DiscussionPosts
                 .Include(dp => dp.User)
                 .Include(dp => dp.ArtistProfile)
-                .Include(dp => dp.Group)
-                .Where(dp => dp.DiscussionPostId == id).FirstOrDefaultAsync();
-
-            var group = await _context.Groups
-                .Include(g => g.Users)
-                .Where(g => g == post.Group).FirstOrDefaultAsync();
-
-            var details = await _context.DiscussionPostsDetails
-                .Where(dpd => dpd.DiscussionPostId == id).FirstOrDefaultAsync();
-            var comments = await _context.Comments
-                .Include(c => c.User)
-                .Where(c => c.DiscussionPostDetailsId == details.DiscussionPostDetailsId).ToListAsync();
-
-            //var type = await _context.UserTypes.Where(u => u.Description == "USER").FirstOrDefaultAsync();
-
-            if (post == null)
-                return NotFound(new { code = "post-not-found" });
-            if (details == null)
-                return NotFound(new { code = "details-not-found" });
-            if (comments == null)
-                return NotFound(new { code = "comments-not-found" });
-            //if (type == null)
-            //    return NotFound(new { code = "user-type-not-found"});
-            var res = new {
-                id = post.DiscussionPostId,
-                author = new {
-                    id = post.UserId,
-                    //name = (post.User.UserTypeId == type.UserTypeId) ? (post.User.UserName) : (string.Empty),
-                    //avatar = (post.User.UserTypeId == type.UserTypeId) ? (post.User.Avatar) : (string.Empty)
-                    name = post.User.UserName,
-                    avatar = !post.User.Avatar.IsNullOrEmpty() ? (post.User.Avatar) : (""),
-                    active = (post.TopicType == DiscussionPost.TopicTypes.Artist) ? (true) : (group.Users.Contains(post.User) ? (true) : (false))
-                },
-                topic = new {
-                    id = (post.TopicType == DiscussionPost.TopicTypes.Artist) ? (post.ArtistProfileId) : (post.GroupId),
-                    name = (post.TopicType == DiscussionPost.TopicTypes.Artist) ? (post.ArtistProfile.Name) : (post.Group.Name),
-                    type = post.TopicType.ToString().ToUpper()
-                },
-                title = post.Title,
-                creationTime = post.CreationTime,
-                numberOfComments = post.NumberOfComments,
-                comments = comments.Select(c => new {
+                .Include(dp => dp.Group.Users)
+                .Include(dp => dp.DiscussionPostDetails.Comments)
+                .Where(dp => dp.DiscussionPostId == id)
+                .Select(d => new {
+                    id = d.DiscussionPostId,
                     author = new {
-                        id = c.UserId,
-                        name = c.User.UserName                  
+                        id = d.UserId,
+                        name = d.User.UserName,
+                        avatar = !d.User.Avatar.IsNullOrEmpty() ? (d.User.Avatar) : (""),
+                        active = (d.TopicType == DiscussionPost.TopicTypes.Artist) ? (true) : (d.Group.Users.Contains(d.User) ? (true) : (false))
                     },
-                    creationTime = c.CreationTime,
-                    content = c.Content
-                }).OrderBy(o => o.creationTime),
-                content = details.Content
-            };
-            return res;
+                    topic = new {
+                        id = (d.TopicType == DiscussionPost.TopicTypes.Artist) ? (d.ArtistProfileId) : (d.GroupId),
+                        name = (d.TopicType == DiscussionPost.TopicTypes.Artist) ? (d.ArtistProfile.Name) : (d.Group.Name),
+                        type = d.TopicType.ToString().ToUpper()
+                    },
+                    title = d.Title,
+                    creationTime = d.CreationTime,
+                    numberOfComments = d.NumberOfComments,
+                    comments = d.DiscussionPostDetails.Comments.Select(c => new {
+                        author = new {
+                            id = c.UserId,
+                            name = c.User.UserName
+                        },
+                        creationTime = c.CreationTime,
+                        content = c.Content
+                    }).OrderBy(o => o.creationTime),
+                    content = d.DiscussionPostDetails.Content
+                })
+                .FirstOrDefaultAsync();
+
+            return post == null ? (BadRequest(new { code = "post-not-found" })) :(post);
         }
         #endregion
 
         #region Album
         [HttpGet("album")]
         public async Task<ActionResult<object>> GetAlbumDetails(Guid id) {
-            if (_context.PremiereAlbums == null || _context.PremiereAlbumDetails == null) {
-                return NotFound();
-            }
-            var details = await _context.PremiereAlbumDetails.Include(pad => pad.PremiereAlbum).Include(pad => pad.Tracks).Where(pad => pad.PremiereAlbumId == id).FirstOrDefaultAsync();
+            if (_context.PremiereAlbums == null || _context.PremiereAlbumDetails == null) return NotFound(new { code = "entity-error" });
 
-            if (details == null) {
-                return NotFound();
-            }
+            var album = await _context.PremiereAlbumDetails
+                .Include(pad => pad.PremiereAlbum)
+                .Include(pad => pad.Tracks)
+                .Where(pad => pad.PremiereAlbumId == id)
+                .Select(a => new {
+                    id = a.PremiereAlbumId,
+                    title = a.PremiereAlbum.Title,
+                    artist = new { 
+                        id = a.PremiereAlbum.ArtistProfileId, 
+                        name = a.PremiereAlbum.ArtistProfile.Name
+                    },
+                    cover = a.PremiereAlbum.Cover,
+                    releaseDate = a.PremiereAlbum.ReleaseDate,
+                    tracks = a.Tracks.Select(t => new {
+                        title = t.Title,
+                        duration = $"{t.Duration:mm\\:ss}"
+                    }),
+                    description = a.Description,
+                    duration = $"{a.Duration:hh\\:mm\\:ss}",
+                    genre = a.Genre,
+                    rating = Math.Round(a.Rating, 2).ToString("0.00")
+                })
+                .FirstOrDefaultAsync();
 
-            var res = new {
-                id = details.PremiereAlbumDetailsId,
-                name = details.PremiereAlbum.Title,
-                cover = details.PremiereAlbum.Cover,
-                releaseDate = details.PremiereAlbum.ReleaseDate,
-                tracks = details.Tracks.Select(t => new {
-                    title = t.Title,
-                    //duration = $"{t.Duration.Minutes}:{t.Duration.Seconds}"
-                    duration = $"{t.Duration:mm\\:ss}"
-                }),
-                description = details.Description,
-                duration = $"{details.Duration:hh\\:mm\\:ss}",
-                genre = details.Genre,
-                rating = Math.Round(details.Rating, 2).ToString("0.00")
-            };
-            return res;
+            return album == null ? (BadRequest(new { code = "album-not-found" })) :(album);
         }
         #endregion
     }
