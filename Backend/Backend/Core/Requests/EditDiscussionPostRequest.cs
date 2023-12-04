9229151cff72f7a779e6fdd14cec62bc2f5ca2f7
Kamil Plewnia <plewnia.k@e80group.com>
2023-12-04 17:39:52 +1100

    [BE] add: add editDPrequest

---
+++ Backend/Backend/Core/Requests/EditDiscussionPostRequest.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Core.Requests {
+    public class EditDiscussionPostRequest {
+        [Required(ErrorMessage = "null-postid")]
+        public Guid PostId { get; set; }
+
+        [Required(ErrorMessage = "null-authorid")]
+        public Guid AuthorId { get; set; }
+
+        [Required(ErrorMessage = "null-data")]
+        public EditDiscussionPostDataEntity Data { get; set; }
+    }
+
+    public class EditDiscussionPostDataEntity {
+        [Required(ErrorMessage = "null-title")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "null-content")]
+        public string Content { get; set; }
+    }
+}
