a98bd48d2e6095fe0a221cec9fca72ab87e20665
Kamil Plewnia <plewnia.k@e80group.com>
2023-12-05 09:08:00 +1100

    [BE] add: add delete post request

---
+++ Backend/Backend/Core/Requests/DeleteDiscussionPostRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Core.Requests {
+    public class DeleteDiscussionPostRequest {
+        [Required(ErrorMessage = "null-postid")]
+        public Guid PostId { get; set; }
+
+        [Required(ErrorMessage = "null-authorid")]
+        public Guid AuthorId { get; set; }
+    }
+}
