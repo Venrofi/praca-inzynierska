033ebf427586e6b282444c93ac6741cc88d243b1
Kamil Plewnia <plewnia.k@e80group.com>
2023-12-04 11:42:56 +1100

    [BE] add: add updateuserrequest

---
+++ Backend/Backend/Core/Requests/UpdateUserRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Core.Requests {
+    public class UpdateUserRequest {
+        [Required(ErrorMessage = "null-memberid")]
+        public Guid MemberId { get; set; }
+
+        [Required(ErrorMessage = "null-bio")]
+        [MaxLength(2000, ErrorMessage = "long-bio")]
+        public string Bio { get; set; } = string.Empty;
+    }
+}
