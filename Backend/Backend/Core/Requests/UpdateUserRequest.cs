41946e2ea16de61caa996a7d13a11762c960da52
Kamil Plewnia <plewnia.k@e80group.com>
2023-12-04 17:26:52 +1100

    [BE] change: change in updateuserrequest, add data

--- Backend/Backend/Core/Requests/UpdateUserRequest.cs
+++ Backend/Backend/Core/Requests/UpdateUserRequest.cs
@@ -5,6 +5,11 @@ namespace Backend.Core.Requests {
         [Required(ErrorMessage = "null-memberid")]
         public Guid MemberId { get; set; }
 
+        [Required(ErrorMessage = "null-data")]
+        public EditUserDataEntity Data { get; set; }
+    }
+
+    public class EditUserDataEntity {
         [Required(ErrorMessage = "null-bio")]
         [MaxLength(2000, ErrorMessage = "long-bio")]
         public string Bio { get; set; } = string.Empty;
