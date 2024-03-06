681fda7c6237338f191c2be50fda84d98c442211
Radosław Terelak <radoslaw.terelak@gmail.com>
2024-03-06 19:29:52 +0100

    Add stub function that uses json-server

---
+++ wortal-app/netlify/functions/stub.js
@@ -0,0 +1,13 @@
+// api.js
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
+
+// Use default router
+server.use(router);
+
+module.exports = server;
